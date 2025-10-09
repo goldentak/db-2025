f5550fa862da2d45654f8bc52c74189135720cb7
= <=>
2025-10-09 13:59:25 +0500

    lab5 4task fix

--- lab5/04_random-function.sql
+++ lab5/04_random-function.sql
@@ -1,4 +1,4 @@
 SELECT 
-    ROUND(RANDOM() * 1000.0, 2) AS random_value
+    ROUND((RANDOM() * 1000)::numeric, 2) AS random_value
 FROM accounts
-WHERE RANDOM() < 0.5;
+WHERE (RANDOM() * 1)::numeric < 0.5;
